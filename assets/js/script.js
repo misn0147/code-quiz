85521249504321ace96ae38e779e3e591748da81
Your Name <natoli.missy@gmail.com>
2021-04-18 08:15:47 -0500

    debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,13 @@ var start = document.querySelector("#start");
 var startingPage = document.querySelector("#starting-page");
 var answer = document.querySelectorAll(".answer-choice");
 var quizPage = document.querySelector("#quiz-page");
-var scoresPage = document.querySelector("scores-page");
+var scoresPage = document.querySelector("#score-input");
 var score = 0;
 var timeLeft = 75;
 var timerEl = document.querySelector("#countdown");
+var submitButton = document.querySelector('#submit');
+var initialsInput = document.querySelector('#initials');
+var number = document.querySelector('#number')
 var choiceA = document.querySelector("#A");
 var choiceB = document.querySelector("#B");
 var choiceC = document.querySelector("#C");
@@ -39,7 +42,7 @@ var questions = [
     choiceD: "square brackets",
     correct : "A" 
 },
-    { q: "String values must be enclosed within _______ when being assigned to variables", 
+    { q: "String values must be enclosed within _______ when being assigned to variables.", 
     choiceA: "commas",
     choiceB: "curly brackets",
     choiceC: "quotes",
@@ -54,36 +57,38 @@ var testObj = questions[runningQuestion];
 
 function startQuiz () {
     startingPage.setAttribute("class", "hide");
+    timerEl.removeAttribute("class", "hide");
     quizPage.removeAttribute("class", "hide");
     renderQuestion();
+    countdown();
 };
 
 function countdown() {
-    var timeInterval = setInteraval(function() {
-        if(timeLeft > 1) {
+    var timeInterval = setInterval(function() {
+        if(timeLeft >= 1) {
             timerEl.textContent = "Time: " + timeLeft;
             timeLeft--;
         }
-        else if (timeLeft === 1) {
-            timerEl.textContent = "Time: " + timeLeft;
-            timeLeft--;
+        else if(runningQuestion > 4) {
+            clearInterval(timeInterval);
+            console.log(timeLeft);
         }
         else {
             timerEl.textContent = '';
             clearInterval(timeInterval);
             endQuiz();
+            console.log(timeLeft);
         }
-    },1000);
+
+    }, 1000);
 }
 
-var finalScore = timeLeft;
-console.log("final score" + finalScore);
+var finalScore = '';
 
 start.onclick = startQuiz;
 
 
 function renderQuestion() {
-    
     document.querySelector("#questions").innerHTML = "<p>" + testObj.q + "</p>";
         choiceA.innerHTML = testObj.choiceA;
         choiceB.innerHTML = testObj.choiceB;
@@ -94,6 +99,9 @@ function renderQuestion() {
     choiceB.addEventListener("click", checkAnswer);
     choiceC.addEventListener("click", checkAnswer);
     choiceD.addEventListener("click", checkAnswer);
+
+    console.log(testObj.q);
+    console.log(runningQuestion);
 }
 
 function checkAnswer (e) {
@@ -110,10 +118,50 @@ function checkAnswer (e) {
     runningQuestion++;
     testObj = questions[runningQuestion]
     renderQuestion();
+
+    if (runningQuestion > 4) {
+        endQuiz();
+        console.log("end");
+        finalScore = timeLeft;
+        console.log("final score is: " + finalScore);
+    }
 }
 
 function endQuiz() {
     quizPage.setAttribute("class", "hide");
     scoresPage.removeAttribute("class", "hide");
+    timerEl.setAttribute("class", "hide");
 }
 
+
+// document.getElementById("number").innerHTML = finalScore;
+
+
+// function renderLastRegistered() {
+//     scoresPage.addAttribute("class", "hide");
+//     timerEl.setAttribute("class", "hide");
+//     var currentWinner = localStorage.getItem("initials, score");
+//   //TODO: if they are null, return early from this function
+//     if (email === null || password === null) {
+//       return;
+//     }
+//   //TODO otherwise set the text of 'userEmailSpan' and 'userPasswordSpan' to the corresponding values from local Storage
+//   userEmailSpan.textContent = initials, score;
+//   }
+
+
+submitButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    
+    var newWinner = document.querySelector('#initials').value;
+    
+    if (newWinner === '') {
+    window.prompt('error', 'Initials cannot be blank');
+    } else {
+    
+    localStorage.setItem("initials, score", newWinner + ", " + finalScore);
+
+    // renderLastRegistered();
+    
+    }
+});
